9e1adefbf791784add95d082fd01c1d0763ea883
Giedrius Bielskis <giedrius.bielskis@tesonet.com>
2018-11-26 13:28:25 +0200

    Map

--- script.js
+++ script.js
@@ -37,7 +37,7 @@
             addTopoData(JSON.parse(this.responseText));
        }
     };
-    xhttp.open("GET", "https://raw.githubusercontent.com/sendmenas/leaflet-map/master/custom.topo.json");
+    xhttp.open("GET", "https://raw.githubusercontent.com/sendmenas/leaflet-map/master/map.topojson");
     xhttp.send();
 
     function addTopoData(topoData) {
